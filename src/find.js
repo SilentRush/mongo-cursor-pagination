8e4e0296b68aa5b6b3be33f4fbbc7ab90ea31eed
Tim Wilkis <twilkis@hotmail.com>
2020-06-13 15:03:30 -0500

    update

--- src/find.js
+++ src/find.js
@@ -34,8 +34,6 @@ module.exports = async function(collection, params) {
   const cursorQuery = generateCursorQuery(params);
   const $sort = generateSort(params);
 
-  console.log('test',cursorQuery,$sort);
-
   // Support both the native 'mongodb' driver and 'mongoist'. See:
   // https://www.npmjs.com/package/mongoist#cursor-operations
   const findMethod = collection.findAsCursor ? 'findAsCursor' : 'find';
